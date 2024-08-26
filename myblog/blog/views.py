ad2c89052959c696d6ea15fb5038491ab9ea804f
alinedaei <86824959+Alinedaei@users.noreply.github.com>
2024-08-26 09:20:15 +0330

    Delete myblog directory

--- myblog/blog/views.py
+++
@@ -1,8 +0,0 @@
-# blog/views.py
-from django.shortcuts import render
-from .models import Post
-
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'index.html', {'posts': posts})
-
